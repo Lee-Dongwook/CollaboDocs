4f62e2dba380564f81532662342361700cc7adf6
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-19 12:30:55 +0900

    feat : Schema

---
+++ server/src/models/document.model.ts
@@ -0,0 +1,52 @@
+import { model, Schema, type Document } from "mongoose";
+
+interface IDocument extends Document {
+  title: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  versions: {
+    content: string;
+    timestamp: Date;
+  }[];
+}
+
+const DocumentSchema = new Schema<IDocument>({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  versions: [
+    {
+      content: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+DocumentSchema.pre("save", function (next) {
+  this.updatedAt = new Date();
+  next();
+});
+
+const DocumentModel = model<IDocument>("Document", DocumentSchema);
+
+export default DocumentModel;
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema, type Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const UserModel = model<IUser>("User", UserSchema);
+
+export default UserModel;
