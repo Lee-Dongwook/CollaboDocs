5be06fe107b1cd5df90c0684a2b219c8b9b54f61
Lee-Dongwook <dlehddnr0713@gmail.com>
2024-10-23 19:14:23 +0900

    feat : DataTable

---
+++ client/src/components/ui/DataTableToolbar/index.tsx
@@ -0,0 +1,52 @@
+import { Input } from "@/components/ui/input";
+import { Table } from "@tanstack/react-table";
+import { DataTableViewOptions } from "@/components/ui/DataTableViewOption";
+import { DataTableFilter } from "@/components/ui/DataTableFilter";
+import { status } from "@/components/ui/filters";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/ui/icon";
+
+interface DataTableToolbarProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTableToolbar<TData>({
+  table,
+}: DataTableToolbarProps<TData>) {
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 items-center space-x-2">
+        <Input
+          placeholder="Search Details..."
+          value={
+            (table.getColumn("fullName")?.getFilterValue() as string) ?? ""
+          }
+          onChange={(event) =>
+            table.getColumn("fullName")?.setFilterValue(event.target.value)
+          }
+          className="h-8 w-[150px] lg:w-[250px]"
+        />
+        {table.getColumn("status") && (
+          <DataTableFilter
+            column={table.getColumn("status")}
+            title="Status"
+            options={status}
+          />
+        )}
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <Icons.cancel className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <DataTableViewOptions table={table} />
+    </div>
+  );
+}
